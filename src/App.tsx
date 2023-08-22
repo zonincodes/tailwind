4a074bfc0505250f2ac0fd1b68c5a0f9f65a9a28
Zonin <business.kariukimartin@gmail.com>
2023-08-22 20:17:09 +0300

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './lib/Navbar';
+import Hero from './lib/Hero';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
